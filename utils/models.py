70a1f7ef62206c81a0e3a9ff95106aad3d9b6f65
mhannani <mohamed_hanani2019@gmail.com>
2021-11-19 16:10:20 +0100

    wrapping up the CNN model.

--- utils/models.py
+++ utils/models.py
@@ -20,7 +20,34 @@ class CNN(nn.Module):
 
         # use a pretrained model as our cnn by tweaking the last layer of the model
         # we gonna use the google's inception model
-        self.inception = models.inception_v3()
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
 
+        # replace the last linear layer
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, embed_size)
 
+        # rectified Linear Unit activation function
+        self.relu = nn.ReLU()
 
+        # setting up DropOut layer
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, images):
+        """
+        The forward pass.
+        :param images: array_like
+            Batch of images
+        :return: array_like
+            The representation of images in [inception.fc.in_features, embed_size]
+        """
+
+        # extract the features from the images
+        features = self.inception(images)
+
+        # make the last linear layer trainable by enabling gradient
+        for name, param in self.inception.named_parameters():
+            if "fc.weight" in name or "fc.bias" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = self.is_train
+
+        return self.dropout(self.relu(features))
