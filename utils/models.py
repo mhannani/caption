720b7d45bba6e9aa1dc35bfa65800fe19c0000e7
mhannani <mohamed_hanani2019@gmail.com>
2021-11-19 18:31:58 +0100

    wrapping up the RNN model.

--- utils/models.py
+++ utils/models.py
@@ -31,6 +31,8 @@ class CNN(nn.Module):
         # setting up DropOut layer
         self.dropout = nn.Dropout(0.5)
 
+        self.fc = self.inception
+
     def forward(self, images):
         """
         The forward pass.
@@ -51,3 +53,56 @@ class CNN(nn.Module):
                 param.requires_grad = self.is_train
 
         return self.dropout(self.relu(features))
+
+
+class RNN(nn.Module):
+    """
+    The Decoder class based on LSTM
+    """
+
+    def __init__(self, embed_size, hidden_size, vocabulary_size, num_layers):
+        """
+        The class constructor of the Recurrent Neural Network
+        :param embed_size: Integer
+            The embedding size
+        :param hidden_size: Integer
+            The hidden cell size of the LSTM
+        :param vocabulary_size: Integer
+            The vocabulary size of the corpus
+        :param num_layers: Integer
+            The number of LSTM cells of the network
+        """
+
+        # call the init function for the super class
+        super().__init__()
+
+        # embedding layer
+        self.embed = nn.Embedding(vocabulary_size, embed_size)
+
+        # the lstm layer
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+
+        # linear layer
+        self.linear = nn.Linear(hidden_size, vocabulary_size)
+
+        # dropout layer
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, features, captions):
+        """
+        Forward pass of the RNN network.
+        :param features: array_like
+            Multidimensional representation of images.
+        :param captions: array_like
+            Representation of captions
+        :return: outputs from each time step of the LSTMs.
+        """
+
+        embeddings = self.dropout(self.embed(captions))
+        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+
+        return outputs
+
+
