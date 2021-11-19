54275f1afe37f35d65fc1f88202a4e452c90cf6a
mhannani <mohamed_hanani2019@gmail.com>
2021-11-20 00:53:14 +0100

    tweaking save_checkpoint function

--- utils/checkpoints.py
+++ utils/checkpoints.py
@@ -3,13 +3,15 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 
-def save_checkpoint(state, filename):
+def save_checkpoint(state, filename, epoch):
     """
     Save model checkpoints
+    :param epoch: epoch
     :param state:
     :param filename:
     :return:
     """
+    filename = "checkpoint_{}_.pth.tar".format(epoch)
     print("==< Saving checkpoint >==")
     torch.save(state, filename)
 
@@ -19,7 +21,7 @@ def load_checkpoint(checkpoint, model, optimizer):
     Load pre-trained model's checkpoints
     :param checkpoint: checkpoint file name
     :param model: model class
-    :param optimized: optimized
+    :param optimizer: optimizer
     :return:
     """
 
