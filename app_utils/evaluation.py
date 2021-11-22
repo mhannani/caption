94e7c931a38bb89110b1881e1c73f0e08c4b2730
mhannani <mohamed_hanani2019@gmail.com>
2021-11-22 17:19:35 +0100

    Setting up BLUE score for model prediction evaluation.

---
+++ app_utils/evaluation.py
@@ -0,0 +1,10 @@
+def blue_score(pred_caption, gt_captions):
+    """
+    Calculate the BLUE score of the generated caption.
+    :param pred_caption: string
+        The generated caption
+    :param gt_captions: string
+        The ground-truth captions
+    :return: double
+        BLUE score value
+    """
