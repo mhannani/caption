5957749dd0992d30eb03631c4881b66e50983aac
mhannani <mohamed_hanani2019@gmail.com>
2021-11-23 01:12:32 +0100

    torchscripting model

--- deployment/utils/torchscript.py
+++ deployment/utils/torchscript.py
@@ -1,6 +1,17 @@
+import torch
+
+
 def torchscript_model(model):
     """
-    Torchscripting the model for optimization with Just-In-Time compiler.
-    :param model:
-    :return:
+    Torchscripts saves the model for optimization with Just-In-Time compiler.
+    :param model: pretrained model
+    :return: torchscripted model
     """
+
+    # torchscript model
+    torchscripted_model = torch.jit.script(model)
+
+    # save the model
+    torchscripted_model.save("../checkpoints_deployment/model.pt")
+
+    return torchscripted_model
